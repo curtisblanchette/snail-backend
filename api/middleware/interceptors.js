d15f1978aea7e9528b3df915d07f738e51d9427d
curtisblanchette <curtis.blanchette88@gmail.com>
2019-03-01 23:22:55 -0800

    add authentication middleware

--- api/middleware/interceptors.js
+++ api/middleware/interceptors.js
@@ -1,15 +1,30 @@
 'use strict';
 
-const logService = require('../services/logService');
+const app = require('../../server');
+const logService = require('../services/log-service');
+const OAuth2Server = require('oauth2-server');
+const Request = OAuth2Server.Request;
+const Response = OAuth2Server.Response;
+
+module.exports.authenticateRequest = (req, res, next) => {
+  const request = new Request(req);
+  const response = new Response(res);
+
+  return app.oauth.authenticate(request, response)
+    .then((token) => {
+      next();
+    }).catch((err) => {
+      res.status(err.code || 500).json(err);
+    });
+};
 
 /**
- * Middleware to log request data to new table
+ * Middleware to log request data to Logs table
  * @param req - request
  * @param res - response
  * @param next - continue
  */
 exports.callLogger = (req, res, next) => {
-  // log the call to mongoDB table
   logService.logRequest(req).then(()=>{
     next();
   }, (err) => {
