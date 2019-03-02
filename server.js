593245f4667a1ecff6707c09d6562bfc82fc3305
curtisblanchette <curtis.blanchette88@gmail.com>
2019-03-01 23:31:36 -0800

    add/register oauth2 routes and models

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000,
-  mongoose = require('mongoose'),
-  ResultModel = require('./api/models/resultModel'), // model must be loaded here
-  LogModel = require('./api/models/logModel'),
-  bodyParser = require('body-parser');
+const express = require('express');
+const app = module.exports = express();
+const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const OAuth2Server = require('oauth2-server');
+const bodyParser = require('body-parser');
+
+// models loaded here
+const ResultModel = require('./api/models/result-model');
+const LogModel = require('./api/models/log-model');
+const ClientModel = require('./api/models/auth/clientModel');
+const TokenModel = require('./api/models/auth/tokenModel');
+const UserModel = require('./api/models/auth/userModel');
+
+const resultRoutes = require('./api/routes/result-routes');
+const logRoutes = require('./api/routes/log-routes');
+const authRoutes = require('./api/routes/auth-routes');
+const userRoutes = require('./api/routes/user-routes');
 
 // mongoose instance connection
 mongoose.Promise = global.Promise;
@@ -14,15 +25,26 @@ mongoose.connect('mongodb://localhost/SnailDB', { useNewUrlParser: true }).then(
   console.log('Error connecting to MongoDB', err);
 });
 
+app.oauth = new OAuth2Server({
+  model: require('./api/services/auth-service'),
+  accessTokenLifetime: 60 * 60,
+  allowBearerTokensInQueryString: true
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// import and register routes
-const resultRoutes = require('./api/routes/resultRoutes');
-resultRoutes(app);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-const logRoutes = require('./api/routes/logRoutes');
+// register routes
+resultRoutes(app);
 logRoutes(app);
+authRoutes(app);
+userRoutes(app);
 
 app.listen(port);
 
