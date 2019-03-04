d3e1ae45b8fbfa8b8b6219a4d8298e43662561af
curtisblanchette <curtis.blanchette88@gmail.com>
2019-03-03 22:07:55 -0800

    remove unused controller method

--- api/controllers/log.controller.js
+++ api/controllers/log.controller.js
@@ -3,24 +3,6 @@
 const logService = require('../services/log.service');
 
 /**
- * Log request
- * @param req
- * @param res
- * @status 200 - call succeeded
- * @status 500 - call failed
- */
-exports.logRequest = (req, res) => {
-  logService.logRequest(req).then((result) => {
-    res.status(200);
-    res.json(result);
-  }, (err) => {
-    res.status(500);
-    res.json({error: err});
-  });
-};
-
-
-/**
  * Get All request logs
  * @param req
  * @param res
